22f4cb3942be699bd7c2e3b7819f5f8fb0484565
Guillaume Laine <guillaumelaine2.0@gmail.com>
2018-11-14 13:49:51 +0100

    promote comps to containers

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-// State and reducers
-import citiesReducer from './reducers/cities_reducer';
-const reducers = combineReducers({
-  cities: citiesReducer
-});
+import { createStore } from 'redux';
+import reducers from './reducers';
+
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
+
 // render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.querySelector('.container')
 );
-
-
